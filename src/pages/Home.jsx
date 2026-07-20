8e78f34805212ac92c9eb159d71a9620982be038
Sumaiyamohideen <sumaiyamohideen@gmail.com>
2026-07-20 22:49:04 +0530

    chore: place About section after Services section on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 /* src/pages/Home.jsx */
 import Hero from '../components/sections/Hero';
 import StatsBar from '../components/sections/StatsBar';
-import About from '../components/sections/About';
 import Services from '../components/sections/Services';
+import About from '../components/sections/About';
 
 export default function Home() {
   return (
     <div data-testid="page-home">
       <Hero />
       <StatsBar />
-      <About />
       <Services />
+      <About />
     </div>
   );
 }
